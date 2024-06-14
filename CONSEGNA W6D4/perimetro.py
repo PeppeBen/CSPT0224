74a6f2afe5ab35c5d6a16ad6d94d5af8587719cc
PeppeBen <bentivegnag@gmail.com>
2024-06-14 20:13:03 +0200

    up

---
+++ CONSEGNA W6D4/perimetro.py
@@ -0,0 +1,18 @@
+scelta = 0
+while(scelta == 0):
+    scelta = int(input("Scegli la figura geometrica: \n1- Quadrato\n2- Rettangolo\n3- Cerchio\n"))
+    if (scelta == 1):
+        lato = int(input("\nScegli la misura del lato: "))
+        perimetro = lato * 4
+        print(f"\nIl perimetro del quadrato è : {perimetro}\n")
+    elif (scelta == 2):
+        base = int(input("\nScegli la misura della base: \n"))
+        altezza = int(input("\nScegli la misura dell'altezza\n"))
+        perimetro = 2 * (base + altezza)
+        print(f"\nIl perimentro del rettangolo è: {perimetro}\n")
+    elif (scelta == 3):
+        raggio = int(input("\nInserisci la misura del raggio\n"))
+        perimetro = 2 * 3.14 * raggio
+        print(f"\nLa circonferenza del cerchio è: {perimetro}\n")
+    else:
+        print("\nScelta non valida, riprova!\n")

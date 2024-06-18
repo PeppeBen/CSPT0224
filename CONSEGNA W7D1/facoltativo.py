98715191ee7d40d993dfcccf10bb14578f8ff963
PeppeBen <bentivegnag@gmail.com>
2024-06-18 18:34:37 +0200

    up

--- CONSEGNA W7D1/facoltativo.py
+++
@@ -1,9 +0,0 @@
-def word_length(A):
-    B = []
-    for word in A:
-        B.append(len(word))
-    return B
-
-A = [input("Inserisci una parola e otterrai la sua lunghezza: \n")]
-B = word_length(A)
-print(B)

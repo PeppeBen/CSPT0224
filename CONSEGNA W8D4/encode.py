d5687c8b063874a9115e330cd7fb9e7a78a19248
PeppeBen <bentivegnag@gmail.com>
2024-06-29 16:20:50 +0200

    UP

--- CONSEGNA W8D4/encode.py
+++
@@ -1,5 +0,0 @@
-from stegano import lsb
-
-secret = lsb.hide("./pippo.jpeg", "Hey Pippo!")
-secret.save("./pippo-secret.png")
-
--- CONSEGNA W8D4/secret.py
+++
@@ -1,4 +0,0 @@
-from stegano import lsb
-
-message = lsb.reveal("./pippo-secret.png")
-print(message)
